66504c0ea2c96289078db1b898de49f6f090766b
He-Martijn <roert.stapje_0o@icloud.com>
2022-01-04 16:23:12 +0100

    Makes 2 Expanded with an image as a child

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
         backgroundColor: Colors.red,
         appBar: AppBar(
           title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.black,
         ),
         body: DicePage(),
       ),
@@ -18,15 +18,20 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Hello world',
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 40.0,
-          fontWeight: FontWeight.bold
-        ),),
-      ),
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          height: double.infinity,
+        ),
+        Expanded(
+          flex: 1, // flex is division ratio 1:1 is default. Just here for reference.
+          child: Image(image: AssetImage('images/dice1.png'),),),
+        Expanded(
+          flex: 1,
+          child: Image(image: AssetImage('images/dice2.png'),),),
+      ],
     );
   }
 }
