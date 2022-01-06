1628e0f078e98bcc8df1d961995d2385c72ffb4d
He-Martijn <roert.stapje_0o@icloud.com>
2022-01-06 19:11:19 +0100

    Create the buttons and declare the variables.

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,10 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Text('Dicee',
+          style: TextStyle(
+              color: Colors.white
+          ),),
           backgroundColor: Colors.black,
         ),
         body: DicePage(),
@@ -18,6 +21,8 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    int leftDiceNumber = 5;
+    int rightDiceNumber = 2;
     return
       SafeArea(
         child: Center(
@@ -34,18 +39,22 @@ class DicePage extends StatelessWidget {
             // Sizedbox isn't necessary. Better use a centre widget.
             Expanded(
               flex: 1, // flex is division ratio 1:1 is default. Just here for reference.
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Image.asset('images/dice4.png'),
+              child: TextButton(
+                onPressed: (){
+                  print('left button pressed');
+                  print('The value for leftDiceNumber = $leftDiceNumber');
+                },
+                  child: Image.asset('images/dice$leftDiceNumber.png'),
               ),
             ),
               //Image(image: AssetImage('images/dice1.png'),),),
             Expanded(
               flex: 1,
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Image.asset('images/dice5.png'),
-              ),
+              child: TextButton(
+                onPressed: (){
+                  print('right button pressed');
+                },
+                  child: Image.asset('images/dice$rightDiceNumber.png')),
               //child: Image(image: AssetImage('images/dice2.png'),),),
             ),
 
